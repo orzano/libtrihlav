e9912eab7c09406907b59a77e78e31997df946d1
Michal Cagarda <inbox@cagarda.com>
2025-01-15 00:08:54 +0100

    Added 'ext' to timer properties

--- include/trh_timer.h
+++ include/trh_timer.h
@@ -15,17 +15,23 @@ typedef struct TTrhTimerProperties {
 	time_t sec;
 	time_t nsec;
 	bool repeat;
+	void *ext;
+
 	handle_event handle_timer_event;
 } TTrhTimerProperties;
 
 
 /**
  * @brief Create a new timer.
+ * @retval TRH_INVALID_ARG iProperties or oEvent is null.
  * @retval TRH_OK
- * 
+ *
  * - allocate memory for timer event
  * - initialize timer 
  * - register timer with epoll
+ * 
+ * If iProperties has been allocated in memory, it can be released after this function.
+ * oEvent must be released with trh_timer_release().
  */
 int trh_timer_init( TTrhTimerProperties *iProperties, TTrhEvent **oEvent );
 
